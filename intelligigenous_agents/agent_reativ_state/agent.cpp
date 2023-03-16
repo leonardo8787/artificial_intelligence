c4230848f28f8066aac8b4768c839bc20115ee66
leonardo8787 <leocampos8787@gmail.com>
2023-03-16 17:12:41 -0300

    atualiza

--- intelligigenous_agents/agent_reativ_state/agent.cpp
+++ intelligigenous_agents/agent_reativ_state/agent.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main(){
     pair<int, int> p[8]={{1,1},{1,0},{0,1},{0,0},{1,1},{1,0},{0,1},{0,0}};
+    vector<pair<int, int>> data;
     int pontuacao=0;
     cout<<"Casos de teste"<<endl;
     for(int i=0; i<8; i++){
-        cout<<"Caso de teste: "<<i<<endl;
+        system("pause"); 
+        cout<<"Caso de teste: "<<i+1<<endl;
         cout<<p[i].first<<" "<<p[i].second<<endl;
         if(i%2==0){
             if(p[i].first == 1){
@@ -19,6 +21,7 @@ int main(){
                 }else{
                     cout<<"Clean"<<endl;
                 }
+                data.push_back({0,0});
             }else if(p[i].first == 0){
                 cout<<"Clean"<<endl;
                 cout<<"Move Right"<<endl;
@@ -28,6 +31,7 @@ int main(){
                 }else{
                     cout<<"Clean"<<endl;
                 }
+                data.push_back({0,0});
             }
         }else{
             if(p[i].second == 1){
@@ -40,6 +44,7 @@ int main(){
                 }else{
                     cout<<"Clean"<<endl;
                 }
+                data.push_back({0,0});
             }else if(p[i].second == 0){
                 cout<<"Clean"<<endl;
                 cout<<"Move Left"<<endl;
@@ -49,11 +54,26 @@ int main(){
                 }else{
                     cout<<"Clean"<<endl;
                 }
+                data.push_back({0,0});
             }
         }
         cout<<endl;
         cout<<"Pontuação: "<<pontuacao<<endl;
         cout<<endl;
     }
+    cout<<"banco de dados"<<endl;
+    for (auto i:data) {
+        cout << i.first<<" "<< i.second << endl;
+        if(i.first!=0){
+            cout<<"Suck"<<endl;
+            pontuacao++;
+            i.first=0;
+        }
+        if(i.second!=0){
+            cout<<"Suck"<<endl;
+            pontuacao++;
+            i.second=0;
+        }
+    }
     return 0;
-}
+} 
---
+++ intelligigenous_agents/agent_reative_simple/agent.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    pair<int, int> p[8]={{1,1},{1,0},{0,1},{0,0},{1,1},{1,0},{0,1},{0,0}};
+    int pontuacao=0;
+    cout<<"Casos de teste"<<endl;
+    for(int i=0; i<8; i++){
+        system("pause"); 
+        cout<<"Caso de teste: "<<i+1<<endl;
+        cout<<p[i].first<<" "<<p[i].second<<endl;
+        if(i%2==0){
+            if(p[i].first == 1){
+                cout<<"Suck"<<endl;
+                cout<<"Move Right"<<endl;
+                pontuacao++;
+                if(p[i].second == 1){
+                    cout<<"Suck"<<endl;
+                    pontuacao++;
+                }else{
+                    cout<<"Clean"<<endl;
+                }
+            }else if(p[i].first == 0){
+                cout<<"Clean"<<endl;
+                cout<<"Move Right"<<endl;
+                if(p[i].second == 1){
+                    cout<<"Suck"<<endl;
+                    pontuacao++;
+                }else{
+                    cout<<"Clean"<<endl;
+                }
+            }
+        }else{
+            if(p[i].second == 1){
+                cout<<"Suck"<<endl;
+                cout<<"Move Left"<<endl;
+                pontuacao++;
+                if(p[i].first == 1){
+                    cout<<"Suck"<<endl;
+                    pontuacao++;
+                }else{
+                    cout<<"Clean"<<endl;
+                }
+            }else if(p[i].second == 0){
+                cout<<"Clean"<<endl;
+                cout<<"Move Left"<<endl;
+                if(p[i].second == 1){
+                    cout<<"Suck"<<endl;
+                    pontuacao++;
+                }else{
+                    cout<<"Clean"<<endl;
+                }
+            }
+        }
+        cout<<endl;
+        cout<<"Pontuação: "<<pontuacao<<endl;
+        cout<<endl;
+    }
+    return 0;
+}
